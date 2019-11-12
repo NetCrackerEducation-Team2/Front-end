98bba63fd4a878e9c94ad9dd1e216fbbf8133068
Volodymyr Balamut <volodymyrbalamut@gmail.com>
2019-11-12 23:42:03 +0200

    Refactoring announcements

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
   AUTH_LOGIN_URL: 'https://netcracker2-back-end.herokuapp.com/auth/login',
   AUTH_REGISTER_URL: 'https://netcracker2-back-end.herokuapp.com/auth/register',
   AUTH_ACTIVATION_URL: 'https://netcracker2-back-end.herokuapp.com/auth/activate/',
-  API_PROFILE: 'http://netcracker2-back-end.herokuapp.com/profile'
+  API_PROFILE: 'http://netcracker2-back-end.herokuapp.com/profile',
+  API_ANNOUNCEMENTS: 'https://netcracker2-back-end.herokuapp.com/api/announcements/'
 };
