f70d9264cc7de6789df0525b98d2f5bb4357ff36
michael <evtushenkomisha21@gmail.com>
2019-11-15 00:04:55 +0200

    change urls to localhost fot tests

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,15 @@
 
 export const environment = {
   production: false,
-  // AUTH_LOGIN_URL: 'http://localhost:8081/auth/login',
-  // AUTH_REGISTER_URL: 'http://localhost:8081/auth/register',
-  // AUTH_ACTIVATION_URL: 'http://localhost:8081/auth/activate/',
-  // API_PROFILE : 'http://localhost:8081/profile',
+  AUTH_LOGIN_URL: 'http://localhost:8081/auth/login',
+  AUTH_REGISTER_URL: 'http://localhost:8081/auth/register',
+  AUTH_ACTIVATION_URL: 'http://localhost:8081/auth/activate/',
+  API_PROFILE : 'http://localhost:8081/profile',
 
-  AUTH_LOGIN_URL: 'http://netcracker2-back-end.herokuapp.com/auth/login',
-  AUTH_REGISTER_URL: 'http://netcracker2-back-end.herokuapp.com/auth/register',
-  AUTH_ACTIVATION_URL: 'http://netcracker2-back-end.herokuapp.com/auth/activate/',
-  API_PROFILE: 'http://netcracker2-back-end.herokuapp.com/profile'
+  // AUTH_LOGIN_URL: 'http://netcracker2-back-end.herokuapp.com/auth/login',
+  // AUTH_REGISTER_URL: 'http://netcracker2-back-end.herokuapp.com/auth/register',
+  // AUTH_ACTIVATION_URL: 'http://netcracker2-back-end.herokuapp.com/auth/activate/',
+  // API_PROFILE: 'http://netcracker2-back-end.herokuapp.com/profile'
 };
 
 /*
