f4e270c6f3ca1c716e8ae06facce6702ae765b31
BondarOleg98 <olegb5195@gmail.com>
2019-11-15 13:46:51 +0200

    Change http to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
 
   API_ANNOUNCEMENTS: 'https://netcracker2-back-end.herokuapp.com/api/announcements/',
-  AUTH_LOGIN_URL: 'http://netcracker2-back-end.herokuapp.com/auth/login',
-  AUTH_REGISTER_URL: 'http://netcracker2-back-end.herokuapp.com/auth/register',
-  AUTH_ACTIVATION_URL: 'http://netcracker2-back-end.herokuapp.com/auth/activate/',
+  AUTH_LOGIN_URL: 'https://netcracker2-back-end.herokuapp.com/auth/login',
+  AUTH_REGISTER_URL: 'https://netcracker2-back-end.herokuapp.com/auth/register',
+  AUTH_ACTIVATION_URL: 'https://netcracker2-back-end.herokuapp.com/auth/activate/',
   API_PROFILE: 'http://netcracker2-back-end.herokuapp.com/profile'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,10 +9,10 @@ export const environment = {
   // AUTH_ACTIVATION_URL: 'http://localhost:8081/auth/activate/',
   // API_PROFILE : 'http://localhost:8081/profile',
 
-  AUTH_LOGIN_URL: 'http://netcracker2-back-end.herokuapp.com/auth/login',
-  AUTH_REGISTER_URL: 'http://netcracker2-back-end.herokuapp.com/auth/register',
-  AUTH_ACTIVATION_URL: 'http://netcracker2-back-end.herokuapp.com/auth/activate/',
-  API_PROFILE: 'http://netcracker2-back-end.herokuapp.com/profile',
+  AUTH_LOGIN_URL: 'https://netcracker2-back-end.herokuapp.com/auth/login',
+  AUTH_REGISTER_URL: 'https://netcracker2-back-end.herokuapp.com/auth/register',
+  AUTH_ACTIVATION_URL: 'https://netcracker2-back-end.herokuapp.com/auth/activate/',
+  API_PROFILE: 'https://netcracker2-back-end.herokuapp.com/profile',
   API_ANNOUNCEMENTS: 'https://netcracker2-back-end.herokuapp.com/api/announcements/'
 };
 
