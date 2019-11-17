fe4a0f437bbf1d6724efcfca309634f9dee1f913
michael <evtushenkomisha21@gmail.com>
2019-11-17 09:33:28 +0200

    url added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,10 +5,12 @@ export const environment = {
   API_BOOK_DOWNLOAD: 'https://netcracker2-back-end.herokuapp.com/api/book/download',
   API_GENRES:  'https://netcracker2-back-end.herokuapp.com/api/genres',
   API_AUTHORS: 'https://netcracker2-back-end.herokuapp.com/api/authors',
+  API_PROFILE: 'https://netcracker2-back-end.herokuapp.com/profile',
   API_ANNOUNCEMENTS: 'https://netcracker2-back-end.herokuapp.com/api/announcements/',
+
   AUTH_LOGIN_URL: 'https://netcracker2-back-end.herokuapp.com/auth/login',
   AUTH_REGISTER_URL: 'https://netcracker2-back-end.herokuapp.com/auth/register',
   AUTH_ACTIVATION_URL: 'https://netcracker2-back-end.herokuapp.com/auth/activate/',
   AUTH_RECOVER_LINK_URL: 'https://netcracker2-back-end.herokuapp.com/auth/recovery-link/',
-  API_PROFILE: 'https://netcracker2-back-end.herokuapp.com/profile'
+  AUTH_RECOVER_PASS_URL: 'https://netcracker2-back-end.herokuapp.com/auth/recover/'
 };
