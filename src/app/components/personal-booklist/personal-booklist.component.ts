e1e272b28f8cbdfbd7d531246e86a9d8ae04f6d0
Anatolii Davydko <anatoly.davydko@gmail.com>
2019-11-24 21:06:31 +0200

    loading logic is implemented.

--- src/app/components/personal-booklist/personal-booklist.component.ts
+++ src/app/components/personal-booklist/personal-booklist.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {AccountService} from '../../service/account.service';
+import {Book} from '../../models/book';
+import {UsersBooksService} from '../../service/users-books-service';
+import {flatMap, map, switchMap} from 'rxjs/operators';
+import {UsersBook} from '../../models/users-book';
+import {Page} from '../../models/page';
+import {BookService} from '../../service/book.service';
 
 @Component({
   selector: 'app-personal-booklist',
@@ -8,15 +14,37 @@ import {AccountService} from '../../service/account.service';
 })
 export class PersonalBooklistComponent implements OnInit {
   userId = 1007;
-  page = 1;
   pageSize = 5;
+  page: number;
+  books: Book[];
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService,
+              private usersBooksService: UsersBooksService,
+              private bookService: BookService,
+  ) { }
 
   ngOnInit() {
+    this.page = 1;
+    this.loadList();
   }
 
   loadList(): void {
-
+    this.usersBooksService.getUsersBookPage(this.userId, this.page, this.pageSize).pipe(
+      map((response: Page<UsersBook> ) => {
+          return response.array;
+      }),
+      flatMap((userBooksList: UsersBook[]) => {
+        return userBooksList;
+      }),
+      switchMap((userBook: UsersBook) => {
+        return this.bookService.getBookById(userBook.bookId);
+      }),
+    ).subscribe( (book: Book) => {
+      this.books.push(book);
+    });
+  }
+  expandReviews(): void {
+    this.page += 1;
+    this.loadList();
   }
 }
