e47b24e4c26b9a5331960e08041f7fbd487118ea
Andrii Semchenko <a7chenko@gmail.com>
2019-11-30 19:29:26 +0200

    Added date formatting on activity list page

--- src/app/components/activity-item/activity-item.component.ts
+++ src/app/components/activity-item/activity-item.component.ts
@@ -1,22 +1,26 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Activity} from '../../models/activity';
 import {ListItemInfo} from '../../models/presentation-models/list-item-info';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-activity-item',
   templateUrl: './activity-item.component.html',
-  styleUrls: ['./activity-item.component.css']
+  styleUrls: ['./activity-item.component.css'],
+  providers: [DatePipe]
 })
 export class ActivityItemComponent implements OnInit {
   @Input() activity: Activity;
   listItemInfo: ListItemInfo;
   theGoBackCallback: () => void;
 
-  constructor() {
-    // TODO asem format date here
+  constructor(private datePipe: DatePipe) {
+  }
+
+  ngOnInit(): void {
     this.listItemInfo = {
       title: this.activity.description,
-      subtitle: this.activity.creationTime,
+      subtitle: this.datePipe.transform(this.activity.creationTime, 'd LLLL yyyy, h:mm'),
       actionElements: null,
       additionalParams: null,
       contentElements: null,
@@ -25,8 +29,5 @@ export class ActivityItemComponent implements OnInit {
       photo: null,
       publish: null
     };
-  }
-
-  ngOnInit(): void {
   }
 }
