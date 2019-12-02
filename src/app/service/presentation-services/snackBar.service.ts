ec3ccd7c482cc01adbf5063c976a926c6a291b3f
Andrii Semchenko <a7chenko@gmail.com>
2019-12-02 03:16:43 +0200

    Added showing errors via snackbar
    Implemented showing caught errors from services.
    Added showing success message after announcement creation

---
+++ src/app/service/presentation-services/snackBar.service.ts
@@ -0,0 +1,47 @@
+import {Injectable} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackBarService {
+
+  constructor(private snackBar: MatSnackBar) {
+  }
+
+  openSuccessSnackBar(message: string): void {
+    this.snackBar.open(message, null, {
+      duration: 3000,
+      panelClass: ['mat-toolbar', 'mat-primary']
+    });
+  }
+
+  openErrorSnackBar(msg: string = 'An error occurred', error: any = null): void {
+    const message = msg + this.getErrorMessage(error);
+    this.snackBar.open(message, null, {
+      duration: 3000,
+      panelClass: ['mat-toolbar', 'mat-warn']
+    });
+  }
+
+  private getErrorMessage(error: any): string {
+    const errorMessage = ((error && error.message) || error || '');
+    // avoid printing error message that provides no information about error cause
+    if (errorMessage.toLowerCase().indexOf('unknown error') >= 0) {
+      return '';
+    }
+    const errorCodeSubstringOffset = errorMessage.includes('Error code: ');
+    if (errorCodeSubstringOffset >= 0) {
+      const code = +errorMessage.substr(errorCodeSubstringOffset + 'Error code: '.length, 3);
+      // tslint:disable-next-line:no-bitwise
+      console.log('Error code: ', 0 | code);
+      switch (code) {
+        case 500:
+          return 'There is problem with our server. Please try again later';
+        // TODO add other cases
+      }
+    }
+    // TODO parse 'Message: ' substring
+    return errorMessage;
+  }
+}
