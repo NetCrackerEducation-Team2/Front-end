78ed1e97c7151b5414cf41c7668d0371a52276ca
Volodymyr Balamut <volodymyrbalamut@gmail.com>
2019-12-18 16:21:13 +0200

    Fixed notification date bug

--- src/app/components/notification/notification-list/notification-list.component.ts
+++ src/app/components/notification/notification-list/notification-list.component.ts
@@ -59,7 +59,7 @@ export class NotificationListComponent implements OnInit {
         } else {
           return {
             title: null,
-            subtitle: this.datePipe.transform(notification.notificationObject.creationTime, 'd LLLL yyyy, h:mm'),
+            subtitle: this.datePipe.transform(notification.notificationObject.creationTime, 'd LLLL yyyy, H:mm'),
             photoPath: null,
             itemId: notification.notificationId,
             publish: null,
